32448e38e613eff24c326d428c7a894fb7aff7fe
Pranto mollick <prantomollick01@gmail.com>
2023-10-03 22:14:42 +0600

    middleware function mongoose

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,4 +1,5 @@
 const Tour = require('../models/tourModel');
+const ApiFeaturers = require('../utils/apiFeatures');
 
 exports.aliasTopTours = (req, res, next) => {
   req.query.limit = 5;
@@ -7,110 +8,8 @@ exports.aliasTopTours = (req, res, next) => {
   next();
 };
 
-class ApiFeaturers {
-  constructor(query, qeuryString) {
-    this.query = query;
-    this.qeuryString = qeuryString;
-  }
-
-  filter() {
-    const queryObj = { ...this.qeuryString };
-    const excludedFields = ['page', 'sort', 'limit', 'fields'];
-    excludedFields.forEach((el) => delete queryObj[el]);
-
-    let queryStr = JSON.stringify(queryObj);
-    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
-
-    this.query = this.query.find(JSON.parse(queryStr));
-    return this;
-  }
-
-  sort() {
-    if (this.qeuryString.sort) {
-      const sortBy = this.qeuryString.sort.split(',').join(' ');
-      this.query = this.query.sort(sortBy);
-    } else {
-      this.query = this.query.sort('-createdAt');
-    }
-    return this;
-  }
-
-  limitFields() {
-    if (this.qeuryString.fields) {
-      const fields = this.qeuryString.fields.split(',').join(' ');
-      this.query = this.query.select(fields);
-    } else {
-      this.query = this.query.select('-__v');
-    }
-    return this;
-  }
-
-  paginate() {
-    const page = this.qeuryString.page * 1 || 1;
-    const limit = this.qeuryString.limit * 1 || 100;
-    const skip = (page - 1) * limit;
-
-    //page=2&limit=10, 1-10,  page 1, 11-20, page 2, 21-30 page 3
-    this.query = this.query.skip(skip).limit(limit);
-    return this;
-  }
-}
-
 exports.getAllTours = async (req, res) => {
   try {
-    console.log(req.query);
-    //Build query
-    // 1A) Filtering
-    // const queryObj = { ...req.query };
-    // const excludedFields = ['page', 'sort', 'limit', 'fields'];
-    // excludedFields.forEach((el) => delete queryObj[el]);
-
-    // 1B) Advanced Filtering
-    // let queryStr = JSON.stringify(queryObj);
-    // queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
-    //{difficulty: 'easy', duration: { gte: 5}}
-    //{difficulty: 'easy', duration: { $gte: 5}}
-    //gte, gt, lte, lt
-
-    // let query = Tour.find(JSON.parse(queryStr));
-
-    // 2) Sorting
-    // if (req.query.sort) {
-    //   const sortBy = req.query.sort.split(',').join(' ');
-    //   query = query.sort(sortBy);
-    // } else {
-    //   query = query.sort('-createdAt');
-    // }
-
-    // 3) Field Limiting
-    // if (req.query.fields) {
-    //   const fields = req.query.fields.split(',').join(' ');
-    //   query = query.select(fields);
-    // } else {
-    //   query = query.select('-__v');
-    // }
-
-    // 4) Pagination
-    // const page = req.query.page * 1 || 1;
-    // const limit = req.query.limit * 1 || 100;
-    // const skip = (page - 1) * limit;
-
-    //page=2&limit=10, 1-10,  page 1, 11-20, page 2, 21-30 page 3
-    // query = query.skip(skip).limit(limit);
-
-    // if (req.query.page) {
-    //   const numTours = await Tour.countDocuments();
-    //   if (skip >= numTours) throw new Error('This page doest not exists!');
-    // }
-
-    // const tours = Tour.find()
-    //   .where('duration')
-    //   .equals(5)
-    //   .where('difficulty')
-    //   .equals('easy');
-
-    //Execute query
-
     const featuers = new ApiFeaturers(Tour.find(), req.query)
       .filter()
       .sort()
@@ -198,6 +97,99 @@ exports.deleteTour = async (req, res) => {
     res.status(204).json({
       status: 'success',
       data: tour,
+    });
+  } catch (error) {
+    res.status(404).json({
+      status: 'Fail',
+      message: error,
+    });
+  }
+};
+
+exports.getTourStats = async (req, res) => {
+  try {
+    const stats = await Tour.aggregate([
+      {
+        $match: { ratingsAverage: { $gte: 4.5 } },
+      },
+      {
+        $group: {
+          _id: { $toUpper: '$difficulty' },
+          numTours: { $sum: 1 },
+          avgRating: { $avg: '$ratingsAverage' },
+          avgPrice: { $avg: '$price' },
+          minPrice: { $min: '$price' },
+          maxPrice: { $max: '$price' },
+        },
+      },
+
+      {
+        $sort: {
+          avgPrice: 1,
+        },
+      },
+
+      // {
+      //   $match: { _id: { $ne: 'EASY' } },
+      // },
+    ]);
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        stats,
+      },
+    });
+  } catch (error) {
+    res.status(404).json({
+      status: 'Fail',
+      message: error,
+    });
+  }
+};
+
+exports.getMonthlyPlan = async (req, res) => {
+  try {
+    const year = req.params.year * 1; //2021
+    const plan = await Tour.aggregate([
+      {
+        $unwind: '$startDates',
+      },
+      {
+        $match: {
+          startDates: {
+            $gte: new Date(`${year}-01-01`),
+            $lte: new Date(`${year}-12-31`),
+          },
+        },
+      },
+      {
+        $group: {
+          _id: { $month: '$startDates' },
+          numTourStarts: { $sum: 1 },
+          tours: { $push: '$name' },
+        },
+      },
+      {
+        $addFields: { month: '$_id' },
+      },
+      {
+        $project: {
+          _id: 0,
+        },
+      },
+      {
+        $sort: { numTourStarts: -1 },
+      },
+      {
+        $limit: 12,
+      },
+    ]);
+    res.status(200).json({
+      status: 'success',
+      data: {
+        plan,
+      },
     });
   } catch (error) {
     res.status(404).json({
--- models/tourModel.js
+++ models/tourModel.js
@@ -1,66 +1,116 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'A tour must have a name'],
-    unique: true,
-    trim: true,
-  },
-  duration: {
-    type: Number,
-    required: [true, 'A tour must have a duration'],
-  },
+const tourSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'A tour must have a name'],
+      unique: true,
+      trim: true,
+    },
+    slug: String,
+    duration: {
+      type: Number,
+      required: [true, 'A tour must have a duration'],
+    },
 
-  maxGroupSize: {
-    type: Number,
-    required: [true, 'A tour must have a group size'],
-  },
+    maxGroupSize: {
+      type: Number,
+      required: [true, 'A tour must have a group size'],
+    },
 
-  difficulty: {
-    type: String,
-    required: [true, 'A tour must have a difficulty'],
-  },
+    difficulty: {
+      type: String,
+      required: [true, 'A tour must have a difficulty'],
+    },
 
-  ratingsAverage: {
-    type: Number,
-    default: 4.5,
-  },
+    ratingsAverage: {
+      type: Number,
+      default: 4.5,
+    },
 
-  ratingsQuantity: {
-    type: Number,
-    default: 0,
-  },
+    ratingsQuantity: {
+      type: Number,
+      default: 0,
+    },
 
-  price: {
-    type: Number,
-    required: [true, 'A tour must have a price'],
-  },
+    price: {
+      type: Number,
+      required: [true, 'A tour must have a price'],
+    },
 
-  priceDiscount: Number,
-  summary: {
-    type: String,
-    trim: true,
-    required: [true, 'A tour must have a description'],
-  },
+    priceDiscount: Number,
+    summary: {
+      type: String,
+      trim: true,
+      required: [true, 'A tour must have a description'],
+    },
 
-  description: {
-    type: String,
-    trim: true,
-  },
+    description: {
+      type: String,
+      trim: true,
+    },
 
-  imageCover: {
-    type: String,
-    required: [true, 'A tour must have a cover image'],
-  },
+    imageCover: {
+      type: String,
+      required: [true, 'A tour must have a cover image'],
+    },
 
-  images: [String],
-  createdAt: {
-    type: Date,
-    default: Date.now(),
-  },
+    images: [String],
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+    },
 
-  startDates: [Date],
+    startDates: [Date],
+    secretTour: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  },
+);
+
+tourSchema.virtual('durationWeeks').get(function () {
+  return this.duration / 7;
+});
+
+// Document Middleware: runs before .save() and .create()
+tourSchema.pre('save', function (next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+// tourSchema.pre('save', function (next) {
+//   console.log('Will save document ...', this);
+//   next();
+// });
+
+// tourSchema.post('save', (doc, next) => {
+//   console.log(doc);
+//   next();
+// });
+
+//Query Middleware
+
+tourSchema.pre(/^find/, function (next) {
+  this.find({ secretTour: { $ne: true } });
+  this.start = Date.now();
+  next();
+});
+
+tourSchema.post(/^find/, function (docs, next) {
+  console.log(`Query took ${Date.now() - this.start} milliseconds!`);
+  next();
+});
+
+tourSchema.pre('aggregate', function (next) {
+  console.log(this);
+  next();
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
