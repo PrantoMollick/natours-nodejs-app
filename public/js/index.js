c8a1a1302b1825d16dec4f648830de6b32505289
Pranto mollick <prantomollick01@gmail.com>
2023-10-20 01:34:43 +0600

    update user password implementation

--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,7 @@
 /* eslint-disable */
 
 import { login, logout } from './login';
-import { updateUserHandler } from './updateSettings';
+import { updateSettings } from './updateSettings';
 import { displayMap } from './mapbox';
 
 import L from 'leaflet';
@@ -10,7 +10,8 @@ import L from 'leaflet';
 const mapBoxEl = document.getElementById('map');
 const loginFormEl = document.querySelector('.form--login');
 const logoutBtnEl = document.querySelector('.nav__el--logout');
-const updateUserFormEl = document.querySelector('.form-user-data');
+const userDataFormEl = document.querySelector('.form-user-data');
+const userPasswordFormEl = document.querySelector('.form-user-password');
 
 //Values
 
@@ -27,5 +28,30 @@ loginFormEl?.addEventListener('submit', (e) => {
   login(email, password);
 });
 
+userDataFormEl?.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const name = document.getElementById('name').value;
+  const email = document.getElementById('email').value;
+  updateSettings({ name, email }, 'data');
+});
+
+userPasswordFormEl?.addEventListener('submit', async (e) => {
+  e.preventDefault();
+  document.querySelector('.btn--save-password').textContent = 'Updating...';
+
+  const passwordCurrent = document.getElementById('password-current').value;
+  const password = document.getElementById('password').value;
+  const passwordConfirm = document.getElementById('password-confirm').value;
+
+  await updateSettings(
+    { passwordCurrent, password, passwordConfirm },
+    'password',
+  );
+
+  document.querySelector('.btn--save-password').textContent = 'Update Password';
+  document.getElementById('password-current').value = '';
+  document.getElementById('password').value = '';
+  document.getElementById('password-confirm').value = '';
+});
+
 logoutBtnEl?.addEventListener('click', (e) => logout());
-updateUserFormEl?.addEventListener('submit', updateUserHandler);
--- public/js/updateSettings.js
+++ public/js/updateSettings.js
@@ -1,24 +1,18 @@
 import axios from 'axios';
 import { showAlert } from './alerts';
 
-export const updateUserHandler = async (e) => {
-  e.preventDefault();
-
-  const name = document.getElementById('name').value;
-  const email = document.getElementById('email').value;
-
+export const updateSettings = async (data, type) => {
   try {
+    const urlType = type === 'password' ? 'updateMyPassword' : 'updateMe';
+
     const res = await axios({
       method: 'PATCH',
-      url: 'http://localhost:3000/api/v1/users/updateMe',
-      data: {
-        name,
-        email,
-      },
+      url: `http://localhost:3000/api/v1/users/${urlType}`,
+      data,
     });
 
     if (res.data.status === 'success') {
-      showAlert('success', 'User Successfully updated!');
+      showAlert('success', `User ${type.toUpperCase()} Successfully updated!`);
     }
   } catch (error) {
     showAlert('error', error.response.data.message);
